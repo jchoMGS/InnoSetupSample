cab810e8d3eed3ee247e1f1646160786e76b9216
Joseph Cho <joseph.cho@winmgs.com>
2025-02-28 15:08:11 -0500

    test

--- InnoSetupWinform/Program.cs
+++ InnoSetupWinform/Program.cs
@@ -11,6 +11,7 @@ namespace InnoSetupWinform
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// test
         [STAThread]
         static void Main()
         {
